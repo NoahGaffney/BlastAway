bb6ab7494c6b0138f7669ab1c98340019d3126e1
NoahGaffney <noahdgaffney@gmail.com>
2025-10-02 19:20:37 -0400

    Initial Commit

---
+++ Blast AwayTests/Blast_AwayTests.swift
@@ -0,0 +1,17 @@
+//
+//  Blast_AwayTests.swift
+//  Blast AwayTests
+//
+//  Created by Noah Gaffney on 10/2/25.
+//
+
+import Testing
+@testable import Blast_Away
+
+struct Blast_AwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
